44c9a3336152e1cdaed48fb5225ebb275ae7f489
Jairo Júnior <jairojjr2003@gmail.com>
2025-04-17 10:33:07 -0300

    feat: created all basics methods for CRUD

--- money-manager/src/main/java/com/example/demo/worker/UserWorker.java
+++ money-manager/src/main/java/com/example/demo/worker/UserWorker.java
@@ -1,6 +1,7 @@
 package com.example.demo.worker;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,21 +21,31 @@ public class UserWorker implements UserWorkerInterface {
     public User save(User user){
         
         if (user.getWallets() != null) {
-        for (Wallet wallet : user.getWallets()) {
-            wallet.setUser(user);
+            for (Wallet wallet : user.getWallets()) {
+                wallet.setUser(user);
+            }
         }
+
+        if (user.getInvestments() != null) {
+            for (Investment investment : user.getInvestments()) {
+                investment.setUser(user);
+            }
+        }
+        return repository.save(user);
     }
 
-    if (user.getInvestments() != null) {
-        for (Investment investment : user.getInvestments()) {
-            investment.setUser(user);
-        }
-    }
-        return repository.save(user);
+    @Override
+    public void delete(Long id){
+        repository.deleteById(id);
     }
 
     @Override
     public List<User> getAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id){
+        return repository.findById(id);
     }
 }
--- money-manager/src/main/java/com/example/demo/worker/UserWorkerInterface.java
+++ money-manager/src/main/java/com/example/demo/worker/UserWorkerInterface.java
@@ -1,10 +1,13 @@
 package com.example.demo.worker;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.model.User;
 
 public interface UserWorkerInterface {
     User save(User user);
+    public void delete(Long id);
     public List<User> getAll();
+    public Optional<User> findById(Long id);
 }
